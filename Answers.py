a634e52ace086908a2cd5f890ee539b85f70c2a1
AbdullahMart <162364327+AbdullahMart@users.noreply.github.com>
2024-04-08 15:31:59 +0200

    Update Answers.py

--- Answers.py
+++ Answers.py
@@ -1,2 +1,6 @@
 # Project Description: In this assignment, you will create a task manager application using the Python programming 
 # language. This application will allow users to add, complete, delete, and list their tasks.
+
+# Project Description: In this assignment, you will create a task manager application using the Python programming 
+# language. This application will allow users to add, complete, delete, and list their tasks. bana bu programi python dilinde yaz
+
